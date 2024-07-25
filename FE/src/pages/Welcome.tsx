86d649a3beb5a149e463772756891f37a546f06e
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-07-26 02:34:10 +0530

    editing welcome page

--- FE/src/pages/Welcome.tsx
+++ FE/src/pages/Welcome.tsx
@@ -9,6 +9,9 @@ const Welcome = () => {
           <div className="text-3xl font-bold">
             "Welcome to the clone of Medium App"
           </div>
+          <div className="text-xl my-2">
+            created by @codewithkara
+          </div>
           <div className="flex justify-center">
             <Link to="/signin" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                 Get Started
