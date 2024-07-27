281bc507379a24e26d7073df4941e1edea982630
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-07-27 22:48:21 +0530

    fixing bug for small screen

--- FE/src/components/Appbar.tsx
+++ FE/src/components/Appbar.tsx
@@ -1,21 +1,27 @@
-import { Link } from 'react-router-dom'
-import { Avatar } from './BlogCard'
+import { Link } from 'react-router-dom';
+import { Avatar } from './BlogCard';
 
 const Appbar = () => {
   let fName = localStorage.getItem("name");
   return (
-    <div className='border-b flex justify-between px-28 py-2 items-center'>
-        <div className='text-3xl font-bold font-[serif] cursor-pointer'>
-            <Link to="/blogs">Medium</Link>
-        </div>
+    <div className='border-b flex justify-between px-4 sm:px-6 md:px-10 lg:px-20 py-2 items-center'>
+      <div className='text-2xl sm:text-3xl font-bold font-[serif] cursor-pointer'>
+        <Link to="/blogs">Medium</Link>
+      </div>
 
-        <div>
-            <Link to="/publish" type="button" className="focus:outline-none text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-xl text-sm px-5 py-2.5 me-5">Add Blog +</Link>
-        
-            <Avatar size= "big" name={fName || "Anonymous"}/>
-        </div>
+      <div className='flex items-center'>
+        <Link
+          to="/publish"
+          type="button"
+          className="focus:outline-none text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-xl text-sm px-3 sm:px-5 py-2 me-2 sm:me-5"
+        >
+          Add Blog +
+        </Link>
+
+        <Avatar size="big" name={fName || "Anonymous"} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Appbar
+export default Appbar;
--- FE/src/components/FullBlog.tsx
+++ FE/src/components/FullBlog.tsx
@@ -1,51 +1,40 @@
-import { Blogs } from "../hooks"
-import Appbar from "./Appbar"
-import { Avatar } from "./BlogCard"
+import { Blogs } from "../hooks";
+import Appbar from "./Appbar";
+import { Avatar } from "./BlogCard";
 
-const FullBlog = ({blog} : {blog: Blogs}) => {
+const FullBlog = ({ blog }: { blog: Blogs }) => {
   return (
     <div>
-        <Appbar/>
-        <div className="flex justify-center">
-            <div className="grid grid-cols-12 px-10 pt-12 w-full max-w-screen-xl">
-                <div className="col-span-8 ">
-                    <div className="text-5xl font-extrabold">
-                        {blog.title}
-                    </div>
-                    <div className="text-slate-500 pt-2">
-                        Posted on 2nd Aug, 2024
-                    </div>
-                    <div className="pt-4">
-                        {blog.content}
-                    </div>
+      <Appbar />
+      <div className="flex justify-center">
+        <div className="grid grid-cols-12 gap-6 px-4 sm:px-6 md:px-10 pt-12 w-full max-w-screen-xl">
+          <div className="col-span-12 lg:col-span-4 order-1 lg:order-2">
+            <div className="text-slate-600 text-lg">Author</div>
+            <div className="flex w-full pt-4">
+              <div className="pr-4 flex flex-col justify-center">
+                <Avatar size={"big"} name={blog.author.name || "Anonymous"} />
+              </div>
+              <div>
+                <div className="text-xl font-bold">
+                  {blog.author.name || "Anonymous"}
                 </div>
-                <div className="col-span-4">
-                    <div className="text-slate-600 text-lg">
-                        Author
-                    </div>
-                    
-                    <div className="flex w-full">
-                        <div className="pr-4 flex flex-col justify-center">
-                            <Avatar size={"big"} name={blog.author.name || "Anonymous"} />
-                        </div>
-                        
-                        <div>
-                            <div className="text-xl font-bold">
-                                {blog.author.name || "Anonymous"}
-                            </div>
-                            <div className="pt-2 text-slate-500">
-                                Random phrase regarding this Anonymous Author art. 
-                            </div>
-                        </div>
-
-                    </div>
-                    
+                <div className="pt-2 text-slate-500">
+                  Random phrase regarding this Anonymous Author art.
                 </div>
+              </div>
             </div>
+          </div>
+          <div className="col-span-12 lg:col-span-8 order-2 lg:order-1">
+            <div className="text-3xl sm:text-4xl md:text-5xl font-extrabold">
+              {blog.title}
+            </div>
+            <div className="text-slate-500 pt-2">Posted on 2nd Aug, 2024</div>
+            <div className="pt-4">{blog.content}</div>
+          </div>
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FullBlog
+export default FullBlog;
