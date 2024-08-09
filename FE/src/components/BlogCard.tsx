d01ef5a7906d464d3c1030f4687839ba5dcba235
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-08-09 20:29:33 +0530

    edit the fontfamily

--- FE/src/components/BlogCard.tsx
+++ FE/src/components/BlogCard.tsx
@@ -24,25 +24,25 @@ const BlogCard = ({ id, authorName, title, content, publishedDate } : BlogCardPr
                   <div className='flex justify-center flex-col'>
                       <Avatar size={'small'} name={authorName}/>
                   </div>
-                  <div className='font-extralight pl-2 flex justify-center flex-col '>
+                  <div className='font-extralight pl-2 flex justify-center flex-col'>
                       {newAuthorName || authorName}
                   </div>
                   <div className='flex justify-center flex-col pl-2'>
                       <Circle/>
                   </div>
-                  <div className='pl-2 font-thin text-slate-500 flex justify-center flex-col text-sm font-cascadia'>
+                  <div className='pl-2 font-thin text-slate-500 flex justify-center flex-col text-sm'>
                   {`${Math.ceil(content.length/100)} min read`}
 
                   </div>
                   
               </div>
-        <div className='text-xl font-semibold mt-2'>
+        <div className='text-xl font-semibold mt-3'>
             {title}
         </div>
         <div className='text-md font-thin'>
             {content.slice(0,100) + "..."}
         </div>
-        <div className='text-slate-500 text-sm font-bold pt-4 font-cascadia'>
+        <div className='text-slate-500 text-sm font-thin pt-4 '>
         {dateString}
 
         </div>
